a440232dfc6886f4ee82c77cc4949f4a8264c515
Geronimo De Abreu <geronimodeabreu@gmail.com>
2019-07-13 22:42:16 -0400

    feat(include?) Implement #include? on Reset

--- ruby/lib/reset.rb
+++ ruby/lib/reset.rb
@@ -2,4 +2,12 @@
 
 class Reset
   VERSION = '1.0.0'
+
+  def initialize(list)
+    @values = list
+  end
+
+  def include?(element)
+    @values.include? element
+  end
 end
--- ruby/test/test_reset.rb
+++ ruby/test/test_reset.rb
@@ -4,5 +4,16 @@ gem 'minitest'
 require "minitest/autorun"
 require "reset"
 
-class TestReset < Minitest::Test
+describe Reset do
+  describe "#include?" do
+    it "returns true when the element exists in the reset" do
+      @reset = Reset.new([1,2,3])
+      assert @reset.include? 1
+    end
+
+    it "returns false when the element doesn't exist in the reset" do
+      @reset = Reset.new([1,2,3])
+      assert !@reset.include?(4)
+    end
+  end
 end
